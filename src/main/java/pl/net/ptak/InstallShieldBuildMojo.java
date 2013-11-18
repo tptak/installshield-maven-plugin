455357fb4c3812412885f6a5933b8ce9782c095e
Tomasz Ptak <tomasz@ptak.net.pl>
2013-11-19 00:18:30 +0100

    Attached Maven Checkstyle Plugin

--- src/main/java/pl/net/ptak/InstallShieldBuildMojo.java
+++ src/main/java/pl/net/ptak/InstallShieldBuildMojo.java
@@ -1,5 +1,24 @@
 package pl.net.ptak;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.File;
 import java.io.IOException;
 
@@ -13,78 +32,107 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-@Mojo(name = "build-is-project")
-public class InstallShieldBuildMojo extends AbstractMojo {
+/**
+ * This is a Mojo class responsible for building the InstallShield project.
+ * 
+ * @author Tomasz Ptak
+ */
+@Mojo( name = "build-is-project" )
+public class InstallShieldBuildMojo
+    extends AbstractMojo
+{
 
-	/**
-	 * Location of the file.
-	 */
-	@Parameter(defaultValue = "${project.build.directory}/output", property = "outputDir", required = true)
-	private File installshieldOutputDirectory;
+    /**
+     * Location of the output of the InstallShield build.
+     */
+    @Parameter( defaultValue = "${project.build.directory}/output", property = "outputDir", required = true )
+    private File installshieldOutputDirectory;
 
-	@Parameter(property = "installshieldProjectFile", defaultValue = "${project.artifactId}.ism", required = true)
-	private File installshieldProjectFile;
+    /**
+     * Location of the InstallShield project file.
+     */
+    @Parameter( property = "installshieldProjectFile", defaultValue = "${project.artifactId}.ism", required = true )
+    private File installshieldProjectFile;
 
-	@Parameter(property = "installshieldExecutable", defaultValue = "ISCmdBld.exe", required = true)
-	private String installshieldExecutable;
+    /**
+     * Where to look for InstallShield executable. By default it is assumed that ISCmdBld.exe is put on the Path
+     * variable
+     */
+    @Parameter( property = "installshieldExecutable", defaultValue = "ISCmdBld.exe", required = true )
+    private String installshieldExecutable;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
+    /**
+     * Verifies that configuration is satisfied to bild the project and builds it.
+     * 
+     * @throws MojoExecutionException when plugin is misconfigured
+     * @throws MojoFailureException when plugin execution result was othere than expected
+     * @see org.apache.maven.plugin.AbstractMojo#execute()
+     */
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
 
-		if (!OS.isFamilyWindows()) {
-			throw new MojoExecutionException(
-					"This plugin is for Windows systems only");
-		}
+        if ( !OS.isFamilyWindows() )
+        {
+            throw new MojoExecutionException( "This plugin is for Windows systems only" );
+        }
 
-		if (!installshieldOutputDirectory.exists()) {
-			installshieldOutputDirectory.mkdirs();
-		}
+        if ( !installshieldOutputDirectory.exists() )
+        {
+            installshieldOutputDirectory.mkdirs();
+        }
 
-		if (!installshieldProjectFile.exists()) {
+        if ( !installshieldProjectFile.exists() )
+        {
 
-			getLog().error(
-					String.format("IS Project File available: %b",
-							installshieldProjectFile.exists()));
-			throw new MojoFailureException(
-					"InstallShield project file not found");
-		}
+            getLog().error( String.format( "IS Project File available: %b", installshieldProjectFile.exists() ) );
+            throw new MojoFailureException( "InstallShield project file not found" );
+        }
 
-		String canonicalProjectFilePath = resolveCanonicalPath(installshieldProjectFile);
-		
-		getLog().info(String.format("About to build file %s", canonicalProjectFilePath));
-		
-		String canonicalOutputDirectoryPath = resolveCanonicalPath(installshieldOutputDirectory);
-		
-		getLog().info(String.format("Output will be placed in %s", canonicalProjectFilePath));
-		
-		CommandLine installshieldCommandLine = new CommandLine(
-				installshieldExecutable);
+        String canonicalProjectFilePath = resolveCanonicalPath( installshieldProjectFile );
 
-		installshieldCommandLine.addArgument("-p");
-		installshieldCommandLine.addArgument(canonicalProjectFilePath);
-		installshieldCommandLine.addArgument("-b");
-		installshieldCommandLine.addArgument(canonicalOutputDirectoryPath);
+        getLog().info( String.format( "About to build file %s", canonicalProjectFilePath ) );
 
-		Executor exec = new DefaultExecutor();
+        String canonicalOutputDirectoryPath = resolveCanonicalPath( installshieldOutputDirectory );
 
-		try {
-			exec.execute(installshieldCommandLine);
-		} catch (IOException e) {
-			String errorMessage = "Failed to execute InstallShield build";
-			getLog().error(errorMessage);
-			getLog().debug("Details to failure: ", e);
-			throw new MojoFailureException(errorMessage);
-		}
-	}
+        getLog().info( String.format( "Output will be placed in %s", canonicalProjectFilePath ) );
 
-	private String resolveCanonicalPath(File file) throws MojoFailureException {
-		try {
-			return file.getCanonicalPath();
-		} catch (IOException e) {
-			String errorMessage = "Failed to resolve canonicalPath for the project file";
-			getLog().error(errorMessage);
-			getLog().debug("Something is wrong with the project file path", e);
-			throw new MojoFailureException(errorMessage);
-		}
-	}
+        CommandLine installshieldCommandLine = new CommandLine( installshieldExecutable );
+
+        installshieldCommandLine.addArgument( "-p" );
+        installshieldCommandLine.addArgument( canonicalProjectFilePath );
+        installshieldCommandLine.addArgument( "-b" );
+        installshieldCommandLine.addArgument( canonicalOutputDirectoryPath );
+
+        Executor exec = new DefaultExecutor();
+
+        try
+        {
+            exec.execute( installshieldCommandLine );
+        }
+        catch ( IOException e )
+        {
+            String errorMessage = "Failed to execute InstallShield build";
+            getLog().error( errorMessage );
+            getLog().debug( "Details to failure: ", e );
+            throw new MojoFailureException( errorMessage );
+        }
+    }
+
+    private String resolveCanonicalPath( File file )
+        throws MojoFailureException
+    {
+        try
+        {
+            return file.getCanonicalPath();
+        }
+        catch ( IOException e )
+        {
+            String errorMessage = "Failed to resolve canonicalPath for the project file";
+            getLog().error( errorMessage );
+            getLog().debug( "Something is wrong with the project file path", e );
+            throw new MojoFailureException( errorMessage );
+        }
+    }
 
 }
