96f235bdfe9b49cbdb52d7f8e8866eff5a79ee6d
Tomasz Ptak <tomasz@ptak.net.pl>
2013-11-24 01:57:50 +0100

    Added maven-executor plugin and started dependency plugin this way

--- src/main/java/pl/net/ptak/PrepareDependenciesMojo.java
+++ src/main/java/pl/net/ptak/PrepareDependenciesMojo.java
@@ -19,9 +19,23 @@ package pl.net.ptak;
  * under the License.
  */
 
+import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.name;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
+
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -38,6 +52,12 @@ public class PrepareDependenciesMojo
     @Parameter( defaultValue = "${project}", readonly = true, required = true )
     private MavenProject project;
 
+    @Parameter( defaultValue = "${session}", readonly = true, required = true )
+    private MavenSession session;
+
+    @Component( role = BuildPluginManager.class )
+    private BuildPluginManager pluginManager;
+
     /**
      * Based on dependencies entered, selects what to copy and what to copy and unpack.
      * 
@@ -48,10 +68,34 @@ public class PrepareDependenciesMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        Xpp3Dom goalConfiguration =
-            project.getGoalConfiguration( "org.apache.maven", "maven-dependency-plugin", "default-copy-dependencies",
-                                          "" );
-        System.out.println( goalConfiguration );
+        executeMojoWithLogs( "org.apache.maven.plugins", "maven-dependency-plugin", "2.8", "copy-dependencies",
+                             configuration() );
+        executeMojoWithLogs( "org.apache.maven.plugins", "maven-dependency-plugin", "2.8", "unpack-dependencies",
+                             configuration(
+                             element( name( "skip" ), "true" )
+                             ) );
+    }
+
+    private void executeMojoWithLogs( String groupId, String artifactId, String version, String goal,
+                                      Xpp3Dom configuration )
+        throws MojoExecutionException
+    {
+        getLog().info( String.format( "--- %s:%s:%s (call within prepare-dependencies) @ empty-project ---",
+                                      artifactId, version, goal ) );
+        getLog().debug( configuration.toString() );
+        executeMojo(
+                     plugin(
+                             groupId( groupId ),
+                             artifactId( artifactId ),
+                             version( version )
+                     ),
+                     goal( goal ),
+                     configuration,
+                     executionEnvironment( project, session, pluginManager ) );
+
+        getLog().info( String.format( "--- %s:%s:%s ended ---",
+                                      artifactId, version, goal ) );
+
     }
 
 }
